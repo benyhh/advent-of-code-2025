a4ec461bbdfcb1838453d8b03d36cc88409e9277
Bendik Nyheim <bendik98ny@gmail.com>
2025-12-15 09:52:12 +0100

    Add functions in day 2 solution

--- solutions/2.py
+++ solutions/2.py
@@ -13,40 +13,37 @@ def is_repetitive(x: str):
     return x1 == x2
     
 
-
-sum_repetitions = 0
-with open('2.txt') as f:
-    line = f.read()
-    
-    ranges = line.split(',')
-    
-    for _range in ranges:
-        start, end = _range.split('-')
+def part1(): 
+    sum_repetitions = 0
+    with open('2.txt') as f:
+        line = f.read()
         
-        # Odd length strings cannot have full repetitions
-        if len(start) == len(end) and len(start) % 2 != 0:
-            continue
+        ranges = line.split(',')
         
-        possible_repetitions = [i for i in range(int(start), int(end)+1) if len(str(i)) % 2 == 0]
-        repetitions = [i for i in possible_repetitions if is_repetitive(str(i))]
-        
-        sum_repetitions += sum(repetitions)
-        
-        
-print(f'Part 1: {sum_repetitions}')
+        for _range in ranges:
+            start, end = _range.split('-')
+            
+            # Odd length strings cannot have full repetitions
+            if len(start) == len(end) and len(start) % 2 != 0:
+                continue
+            
+            possible_repetitions = [i for i in range(int(start), int(end)+1) if len(str(i)) % 2 == 0]
+            repetitions = [i for i in possible_repetitions if is_repetitive(str(i))]
+            
+            sum_repetitions += sum(repetitions)
+            
+    print(f'Part 1: {sum_repetitions}')
 
 
 
 def has_repetitive_pattern(x: str):
     """
     For a given integer string, detect depetitive digit patterns of any length.
-    
     We can do that by checking all possible splits of the string.
     """
-    for i in range (1, len(x)):
+    for i in range(1, len(x)):
         if len(x) % i != 0:
             continue
-        
         segments = [x[j:j+i] for j in range(0, len(x), i)]
         if len(set(segments)) == 1:
             return True
@@ -54,19 +51,25 @@ def has_repetitive_pattern(x: str):
     return False
 
 
-sum_repetitions = 0
-with open('2.txt') as f:
-    line = f.read()
+
+def part2():
+    sum_repetitions = 0
+    with open('2.txt') as f:
+        line = f.read()
+        
+        ranges = line.split(',')
+        
+        for _range in ranges:
+            start, end = _range.split('-')
+            
+            repetitions = [i for i in range(int(start), int(end)+1) if has_repetitive_pattern(str(i))]
+            sum_repetitions += sum(repetitions)
+                    
+    print(f'Part 2: {sum_repetitions}')
     
-    ranges = line.split(',')
+
     
-    for _range in ranges:
-        start, end = _range.split('-')
-        
-        repetitions = [i for i in range(int(start), int(end)+1) if has_repetitive_pattern(str(i))]
-        
-        sum_repetitions += sum(repetitions)
-        
-        
-print(f'Part 2: {sum_repetitions}')
+if __name__ == "__main__":
+    part1()
+    part2()
 
